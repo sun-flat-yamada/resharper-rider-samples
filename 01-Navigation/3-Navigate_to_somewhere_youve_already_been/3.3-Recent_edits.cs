d77004c8463661be34e5b23e759be33fd0ca8ec8
sun.flat.yamada <sun.flat.yamada@hotmail.com>
2021-05-03 18:49:03 +0900

    Translate to Japanese

--- 01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.3-Recent_edits.cs
+++ 01-Navigation/3-Navigate_to_somewhere_youve_already_been/3.3-Recent_edits.cs
@@ -9,6 +9,12 @@ namespace JetBrains.ReSharper.Koans.Navigation
     // <shortcut id="View Recent Edits">Ctrl+Shift+, (ReSharper VisualStudio Keymap)</shortcut>
     // <shortcut id="Recent Locations" >Ctrl+Shift+E (Rider Default IntelliJ Keymap)</shortcut>
     //
+    // 最近の編集内容をポップアップメニューで表示出来ます。
+    // (ReSharper) ファイルではなくコードブロックが表示出来ます。
+    //
+    // <shortcut id="Recently Changed Files">Ctrl+Shift+,    (ReSharper VisualStudio Keymap)</shortcut>
+    // <shortcut id="Recently Changed Files">Ctrl+E (Toggle) (Rider Default IntelliJ Keymap)</shortcut>
+    //
 
     public class RecentEdits
     {
@@ -26,6 +32,9 @@ namespace JetBrains.ReSharper.Koans.Navigation
             // 1. Implement GetName by returning firstName
             //    Uncomment the following and comment Exception line
             // return firstName;
+            // 1. firstName を返す GetName を実装してください。
+            //    コメントを外して Exception の行はコメントアウトしてください。
+            // return firstName;
             throw new NotImplementedException();
         }
 
@@ -42,6 +51,11 @@ namespace JetBrains.ReSharper.Koans.Navigation
             //     NOTE: There is no great significance to this implementation.
             //           It is content to let you experience the feel
             //           of the navigation controls.
+            // 3. GetName に ”Recent Edits" を使って戻り、 firstName + lastName に実装を変更します。
+
+            // 4. ここに戻って以下のコメントを外してください。
+            //    (この実装に大きな意味はありません。移動の操作感を体験してもらう内容です。)
+            //TODO: Riderではこの演習は機能しない。コードブロック表示ではなくファイルリストで、自ファイルは表示されない。
             // Console.WriteLine("Done");
         }
     }
